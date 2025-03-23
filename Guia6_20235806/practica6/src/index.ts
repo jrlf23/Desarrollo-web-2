e03cb7a9c2f325383b2bf8695f68384aae2478d5
Jaime Lazo <lazoferman.jaime@gmail.com>
2025-03-23 15:12:05 -0600

    Guia 6 update 1.3

--- Guia6_20235806/practica6/src/index.ts
+++ Guia6_20235806/practica6/src/index.ts
@@ -1,11 +1,15 @@
 import express from "express"
 import router from "./routes"
+import { connectBD } from "./config/db";
+
 
 const PORT = 3000;
 const app=express();
 
 app.use(express.json());
 app.use('/',router);
+
+connectBD();
 
 app.listen(PORT, ()=>{
     console.log(`Servidor corriendo en el puerto ${PORT}`);
